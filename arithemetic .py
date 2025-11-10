834c0db90588c13418ec2f1b5abf538ff825e84c
arundhatisaunshi <arundhatisaunshi@gmail.com>
2025-11-10 16:43:06 +0530

    Update arithemetic .py

--- arithemetic .py
+++ arithemetic .py
@@ -1,1 +1,10 @@
-
+a=int(input("enter your age"))
+weight=float(input("enter your weight"))
+print("the age is", a)
+print("the weight is",weight)
+add=a+weight
+sub=a-weight
+mul=a*weight
+div=a/weight 
+div1=a%weight
+print(add,sub,mul,div,div1)
